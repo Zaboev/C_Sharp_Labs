02857d15a06f0f237ec6eb67aef7ea212dfca80d
Zaboev <148198802+Zaboev@users.noreply.github.com>
2024-09-10 18:52:34 +0300

    Add files via upload

---
+++ DataValidatorLibrary/ValidationRules.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace DataValidatorLibrary  
+{
+    public class NotNullRule<T> : IValidationRule<T>  
+    {
+        public void Validate(T data)  
+        {
+            if (data == null) 
+            {
+                throw new ValidationException("Data cannot be null."); 
+            }
+        }
+    }
+
+    public class StringLengthRule : IValidationRule<string>  
+    {
+        private readonly int _min;  
+        private readonly int _max; 
+
+        public StringLengthRule(int min, int max)  
+        {
+            _min = min;  
+            _max = max;  
+        }
+
+        public void Validate(string data) 
+        {
+            if (data == null)  
+            {
+                throw new ValidationException("Data cannot be null.");  
+            }
+
+            if (data.Length < _min || data.Length > _max) 
+            {
+                throw new ValidationException($"String length must be between {_min} and {_max} characters.");  
+            }
+        }
+    }
+
+    public class PositiveNumberRule : IValidationRule<int>  
+    {
+        public void Validate(int data)  
+        {
+            if (data <= 0) 
+            {
+                throw new ValidationException("Number must be positive.");  
+            }
+        }
+    }
+}
---
+++ DataValidatorLibrary/ValidatorBuilder.cs
@@ -0,0 +1,75 @@
+﻿using System;  
+using System.Collections.Generic;  
+using System.Linq;  
+
+namespace DataValidatorLibrary 
+{
+    
+    public interface IValidationRule<T>
+    {
+        
+        void Validate(T data);
+    }
+
+    public class ValidatorBuilder<T>
+    {
+        
+        private readonly List<IValidationRule<T>> _rules = new List<IValidationRule<T>>();
+
+        
+        public ValidatorBuilder<T> AddRule(IValidationRule<T> rule)
+        {
+            _rules.Add(rule);  
+            return this;  
+        }
+
+       
+        public DataValidator<T> Build()
+        {
+            if (!_rules.Any())
+            {
+                throw new InvalidOperationException("No validation rules added.");
+            }
+
+            return new DataValidator<T>(_rules);
+        }
+    }
+
+    public class DataValidator<T>
+    {
+        private readonly IEnumerable<IValidationRule<T>> _rules;
+
+     
+        public DataValidator(IEnumerable<IValidationRule<T>> rules)
+        {
+            _rules = rules;  
+        }
+
+        public void Validate(T data)
+        {
+            var exceptions = new List<Exception>();
+
+            foreach (var rule in _rules)
+            {
+                try
+                {
+                    rule.Validate(data);
+                }
+                catch (Exception ex)
+                {
+                    exceptions.Add(ex);
+                }
+            }
+
+            if (exceptions.Any())
+            {
+                throw new AggregateException(exceptions);
+            }
+        }
+    }
+
+    public class ValidationException : Exception
+    {
+        public ValidationException(string message) : base(message){ }
+    }
+}
